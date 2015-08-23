52925d475c9a35a57517b6ccb7753b391e5f7354
Elliott Mahler <join.together@gmail.com>
2015-08-23 12:34:21 -0700

    added code to spawn a copy

---
+++ Assets/Scripts/SpawnCopy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnCopy : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject copyPrefab;
+
+	[SerializeField]
+	private float duration;
+
+	private float timeRemaining;
+
+	// Use this for initialization
+	void Start () {
+		timeRemaining = duration;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		timeRemaining -= Time.fixedDeltaTime;
+
+		if (timeRemaining <= 0f) {
+			Instantiate(copyPrefab, this.transform.position, Quaternion.identity);
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/SpawnCopyPlaceholder.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnCopyPlaceholder : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject placeholderPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Instantiate(placeholderPrefab, this.transform.position, Quaternion.identity);
+		}
+	}
+}
