5896ea4dc6fead3f2c2ae61e4734c0447e3a690d
liJIREN <George@liJIRENs-MacBook-Pro.local>
2015-08-23 11:44:55 -0700

    coneVisionFirstVersion

---
+++ Assets/Scripts/ConeVision.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConeVision : MonoBehaviour {
+	private float radius=1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void CheckForObjectsWithinRange()
+	{
+		foreach (GameObject b in GameMaster.g.Bunnies)
+		{
+			if(Vector2.Distance(b.transform.position,transform.position)<radius)
+			{
+				GameMaster.g.RemoveEntity(b);
+			}
+		}
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GameMaster : MonoBehaviour {
+	[HideInInspector]
+	public List <GameObject> Bunnies = new List<GameObject>();
+	List <GameObject> BunniesToRemove = new List<GameObject>();
+
+	public static GameMaster g;
+	// Use this for initialization
+	void Start () {
+		if (g == null)
+		{
+			g = this;
+		}
+		else
+		{
+			Destroy(this);
+		}
+		FindAllBunnies ();
+	}
+	private void FindAllBunnies()
+	{
+		Bunnies=GameObject.FindGameObjectsWithTag("Bunny").ToList();
+
+	}
+
+
+	public void RemoveEntity(GameObject b)
+	{
+		BunniesToRemove.Add (b);
+		GameObject.Destroy (b);
+	}
+	IEnumerator RemoveEntitiesFromList()
+	{
+		yield return new WaitForEndOfFrame();
+		foreach(GameObject b in BunniesToRemove)
+		{
+			if(b!=null)
+			{
+				Bunnies.Remove (b);
+			}
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine (RemoveEntitiesFromList ());
+	}
+}
