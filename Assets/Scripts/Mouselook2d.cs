0e7bccfe7faee5583a41d7ae67b91349287610d4
Elliott Mahler <join.together@gmail.com>
2015-08-23 11:00:57 -0700

    added mouseloook script

---
+++ Assets/Scripts/Mouselook2d.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouselook2d : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
